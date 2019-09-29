3c6aa641c2e3ccfa28dcaa4f0778cc86ae01a105
dasoncheng <dasoncheng@foxmail.com>
2019-09-29 09:34:24 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,34 @@
-import PDFDocument from "pdfkit"
-import * as fs from "fs"
+import PDFDocument from "pdfkit";
+import * as fs from "fs";
 
-import Global from "./global"
-import mm from "./mm"
+import Global from "./global";
+import mm from "./mm";
+import { join } from "path";
 
 //New Document
-console.clear()
-console.log("--> Creating new Testing Document...")
-const dcto = new PDFDocument({ size: [ mm(150), mm(100) ], margin: mm(0) })
-dcto.pipe(fs.createWriteStream(Global.Path.pdfFile))
+console.clear();
+console.log("--> Creating new Testing Document...");
+const dcto = new PDFDocument({ size: [mm(150), mm(100)], margin: mm(0) });
+dcto.pipe(fs.createWriteStream(join(__dirname, "../out.pdf")));
 
 //Title
-dcto.font(Global.Path.Fonts.notoSansSinplif)
-dcto.fontSize(10)
+dcto.font(join(__dirname, "../source/fonts/苹方黑体-准-简.ttf"));
+dcto.fontSize(10);
 dcto.text(
-    "冷冻去骨牛肉 / Frozen Boneless Beef / Carne Bovina sin Hueso Congelada", 
-    mm(2), mm(1),
-    {
-        width: mm(145)
-    }
-)
+  "冷冻去骨牛肉 / Frozen Boneless Beef / Carne Bovina sin Hueso Congelada",
+  mm(2),
+  mm(1),
+  {
+    width: mm(145)
+  }
+);
 
 //Thats the Issue ↓↓↓
-dcto.fontSize(7)
-dcto.text("产品名:", 
-    mm(2), mm(7),
-    { stroke: false }
-)
-dcto.text(
-    "Product Name:",
-    mm(2), mm(10),
-    { stroke: false }
-)
-dcto.text(
-    "Nombre Producto:",
-    mm(2), mm(13),
-    { stroke: false }
-)
+dcto.fontSize(7);
+dcto.text("产品名:", mm(2), mm(7), { stroke: false });
+dcto.text("Product Name:", mm(2), mm(10), { stroke: false });
+dcto.text("Nombre Producto:", mm(2), mm(13), { stroke: false });
 //Thats the Issue ↑↑↑
 
-console.log("--> Document Done!")
-dcto.end()
+console.log("--> Document Done!");
+dcto.end();
